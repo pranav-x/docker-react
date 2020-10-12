1b94f43c167f648628ff4a20638cc12c47bcbe7d
pranav-x <pranav.gautam82@gmail.com>
2020-10-12 23:25:11 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hellosdsd  ds ds there <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Bye there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Lei was chascnft
+          Learn React
         </a>
       </header>
     </div>
